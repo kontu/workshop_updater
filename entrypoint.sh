f15684911de7165c6a3d49a019fffd2c90bfdc41
Kontu <bartschb@gmail.com>
2022-04-12 14:43:04 -0400

    debug

--- entrypoint.sh
+++ entrypoint.sh
@@ -13,12 +13,14 @@ echo "SteamAcct:: $2"
 
 mods=$(printf '%s\n' "${INPUT_FILES[@]}" | cut -f1-2 -d '/' | uniq)
 
-echo "Mods:: $mods"
+
 for item in $mods
+echo "Item:: $item"
 do
     if [[ $item == $1* ]]; 
     then 
         upload=$(find /mnt/c/Users/kontu/scripts/workshop_updater_test/$item -name "*.vdf" )
+        echo "Upload:: $upload"
         steamcmd +login "$2" "$3" +workshop_build_item "$upload" +quit
     fi
 done
