bafb220d82834cd8d9360feee5fae351a4e4ef08
Kontu <bartschb@gmail.com>
2022-04-13 08:07:06 -0400

    k

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,9 @@ set -x # echo on
 apt update && apt install -y git
 git config --global --add safe.directory /github/workspace
 cd $GITHUB_WORKSPACE
-files=$(git diff-tree --no-commit-id --name-only -r $GITHUB_SHA | xargs)
+list=$(git diff-tree --no-commit-id --name-only -r $GITHUB_SHA | xargs)
+# Turns list of files to an array bc it didn't want to do it in one operation
+files=($list)
 mods=$(printf '%s\n' "${files[@]}" | cut -f1-2 -d '/' | uniq)
 mkdir -p "/root/.steam/config/"
 
@@ -15,18 +17,13 @@ echo "$INPUT_SSFNCONTENTS" | base64 -d > "/root/.steam/$INPUT_SSFNFILENAME"
 echo "$INPUT_STEAMCONFIGVDF" | base64 -d > "/root/.steam/config/config.vdf"
 
 
-echo "List of changed files: "
-for i in $files
-do
-    echo $i
-done
 
 # Run through updating the mods if the above parsed correctly
-for item in $mods
+for mod in $mods
 do
-    if [[ $item == $INPUT_PATH* ]]; 
+    if [[ $mod == $INPUT_PATH* ]]; 
     then 
-        echo "Mod:: $item"
+        echo "Mod:: $mod"
         upload=$(find $GITHUB_WORKSPACE/$item -name "*.vdf" )
         echo "Upload:: $upload"
         #steamcmd +login "$INPUT_STEAMACCT" "$INPUT_STEAMPASSWD" +workshop_build_item "$upload" +quit
