3c9702edb223118b314f0b2571e7a6e766638b2d
Kontu <bartschb@gmail.com>
2022-04-13 12:16:19 -0400

    murder.

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,17 +7,15 @@ list=$(git diff-tree --no-commit-id --name-only -r $GITHUB_SHA | xargs)
 # Turns list of files to an array bc it didn't want to do it in one operation
 files=($list)
 mods=$(printf '%s\n' "${files[@]}" | cut -f1-2 -d '/' | uniq)
-mkdir -p "/github/home/.steam/config/"
+mkdir -p "/github/home/Steam/config/"
 
 echo "GithubSHA:: $GITHUB_SHA"
-echo "Files:: $files"
+echo "Changed Files:: $list"
 echo "Path:: $INPUT_PATH"
 echo "SteamAcct:: $INPUT_STEAMACCT"
 echo "SSFN Filename:: $INPUT_SSFNFILENAME"
-echo "$INPUT_SSFNCONTENTS" | base64 -d > "/github/home/.steam/$INPUT_SSFNFILENAME"
-echo "$INPUT_STEAMCONFIGVDF" | base64 -d > "/github/home/.steam/config/config.vdf"
-
-
+echo "$INPUT_SSFNCONTENTS" | base64 -d > "/github/home/Steam/$INPUT_SSFNFILENAME"
+echo "$INPUT_STEAMCONFIGVDF" | base64 -d > "/github/home/Steam/config/config.vdf"
 
 # Run through updating the mods if the above parsed correctly
 for mod in $mods
