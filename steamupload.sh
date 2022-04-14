1850df1eddc1f66dcba073107903b103be916a00
Kontu <bartschb@gmail.com>
2022-04-14 08:50:25 -0400

    less debug

--- steamupload.sh
+++ steamupload.sh
@@ -56,9 +56,6 @@ do
             echo "#             Errors            #"
             echo "#################################"
             echo ""
-            find / -type f -iname  "*libraryfolders.vdf" 2>/dev/null
-            find / -type f -iname  "*config.vdf" 2>/dev/null
-
             echo "Listing steam home contents"
             ls -Ralph $STEAM_HOME
             echo ""
