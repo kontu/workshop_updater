ac8a3ac492b1b90498c705b83d84fc9591957266
Kontu <bartschb@gmail.com>
2022-04-14 10:47:41 -0400

    honestly who puts a variable there

--- steamupload.sh
+++ steamupload.sh
@@ -24,18 +24,17 @@ echo ""
 mkdir -p "$STEAM_HOME/config"
 mkdir -p "/home/runner/Steam/config"
 
-echo "Copying $STEAM_HOME/config/config.vdf"
-set +x
-echo "$SteamConfigVDF" > "$STEAM_HOME/config/config.vdf"
-chmod 777 "$STEAM_HOME/config/config.vdf"
+# echo "Copying $STEAM_HOME/config/config.vdf"
+# echo "$SteamConfigVDF" > "$STEAM_HOME/config/config.vdf"
+# chmod 777 "$STEAM_HOME/config/config.vdf"
 
 echo "Copying /home/runner/Steam/config/config.vdf"
 echo "$SteamConfigVDF" > "/home/runner/Steam/config/config.vdf"
 chmod 777 "/home/runner/Steam/config/config.vdf"
 
-echo "Copying $STEAM_HOME/ssfn"
-echo "$ssfnContents" | base64 -d > "$STEAM_HOME/$ssfnFileName"
-chmod 777 "$STEAM_HOME/$ssfnFileName"
+# echo "Copying $STEAM_HOME/ssfn"
+# echo "$ssfnContents" | base64 -d > "$STEAM_HOME/$ssfnFileName"
+# chmod 777 "$STEAM_HOME/$ssfnFileName"
 
 echo "Copying /home/runner/Steam/ssfn"
 echo "$ssfnContents" | base64 -d > "/home/runner/Steam/$ssfnFileName"
@@ -51,7 +50,7 @@ do
         echo "Mod to upload:: $mod"
         upload=$(find $GITHUB_WORKSPACE/$mod -name "*.vdf" )
         echo "Upload VDF File:: $upload"
-        sed -i 's/"contentfolder" "/"contentfolder" "$GITHUB_WORKSPACE\\/g' $upload
+        sed -i 's/"contentfolder" "/"contentfolder" "'$GITHUB_WORKSPACE'\\/g' $upload
         cat $upload
         echo ""
         $STEAM_CMD +login "$steamAcct" "$steamPasswd" +workshop_build_item "$upload" +quit || (
