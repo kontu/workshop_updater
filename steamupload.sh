e937ead083654ec28280ae5157d627b669c4465b
Kontu <bartschb@gmail.com>
2022-04-14 12:03:03 -0400

    It works, I did some cleanup.

--- steamupload.sh
+++ steamupload.sh
@@ -1,45 +1,36 @@
 #!/bin/bash
-#set -x # echo on for debug
 
 cd $GITHUB_WORKSPACE
+# Checks what files were modified
 list=$(git diff-tree --no-commit-id --name-only -r $GITHUB_SHA | xargs)
+
 # Turns list of files to an array bc it didn't want to do it in one operation
 files=($list)
 mods=$(printf '%s\n' "${files[@]}" | cut -f1-2 -d '/' | uniq)
 
+
+echo "### Environment Information ###"
 echo "GithubWorkspace:: $GITHUB_WORKSPACE"
 echo "GithubSHA:: $GITHUB_SHA"
 echo "Changed Files:: $list"
 echo "Path:: $path"
 echo "SteamAcct:: $steamAcct"
 echo "SSFN Filename:: $ssfnFileName"
-echo "Steam_Home:: $STEAM_HOME"
 echo "Steam_executable:: $STEAM_CMD"
 echo ""
+
 echo "#################################"
 echo "#    Copying SteamGuard Files   #"
 echo "#################################"
 echo ""
-
-mkdir -p "$STEAM_HOME/config"
 mkdir -p "/home/runner/Steam/config"
-
-# echo "Copying $STEAM_HOME/config/config.vdf"
-# echo "$SteamConfigVDF" > "$STEAM_HOME/config/config.vdf"
-# chmod 777 "$STEAM_HOME/config/config.vdf"
 
 echo "Copying /home/runner/Steam/config/config.vdf"
 echo "$SteamConfigVDF" > "/home/runner/Steam/config/config.vdf"
-chmod 777 "/home/runner/Steam/config/config.vdf"
-
-# echo "Copying $STEAM_HOME/ssfn"
-# echo "$ssfnContents" | base64 -d > "$STEAM_HOME/$ssfnFileName"
-# chmod 777 "$STEAM_HOME/$ssfnFileName"
-
 echo "Copying /home/runner/Steam/ssfn"
 echo "$ssfnContents" | base64 -d > "/home/runner/Steam/$ssfnFileName"
 chmod 777 "/home/runner/Steam/$ssfnFileName"
-
+chmod 777 "/home/runner/Steam/config/config.vdf"
 echo "Finished Copying SteamGuard Files!"
 echo ""
 # Run through updating the mods if the above parsed correctly
@@ -54,41 +45,16 @@ do
         sed -i "s|\\\|\/|g" $upload
         sed -i  "s|\"contentfolder\" \"|\"contentfolder\" \"$var\\/|g" $upload
         sed -i  "s|\"previewfile\" \"|\"previewfile\" \"$var\\/|g" $upload
-        sed -i  "s|\"title\" \"|\"title\" \"$var\\/|g" $upload
         
         cat $upload
         echo ""
         $STEAM_CMD +login "$steamAcct" "$steamPasswd" +workshop_build_item "$upload" +quit || (
-            cat "/home/runner/Steam/logs/stderr.txt"
-        )
-        #  || (
-        #     echo ""
-        #     echo "#################################"
-        #     echo "#             Errors            #"
-        #     echo "#################################"
-        #     echo ""
-        #     echo "Listing steam home contents"
-        #     ls -Ralph $STEAM_HOME
-        #     echo ""
-        #     echo "Listing current folder and rootpath"
-        #     echo ""
-        #     ls -alh
-        #     echo ""
-        #     ls -alh $rootPath
-        #     echo ""
-        #     echo "Listing logs folder:"
-        #     echo ""
-        #     ls -Ralph "/home/runner/Steam/logs/"
-        #     echo ""
-        #     echo "Displaying error log"
-        #     echo ""
-        #     cat "/home/runner/Steam/logs/stderr.txt"
-        #     echo ""
-        #     echo "Displaying bootstrapper log"
-        #     echo ""
-        #     cat "/home/runner/Steam/logs/bootstrap_log.txt"
-        #     echo ""
-        #     exit 1
-        # )
+            echo ""
+            echo "#################################"
+            echo "#             Error             #"
+            echo "#################################"
+            echo ""
+            echo "MOD failed to upload please review output above"
+            )
     fi
 done
